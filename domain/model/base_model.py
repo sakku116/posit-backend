195b39ee2d3f786175e8efb5980723c0ffecca5b
zakky <zakky1164@gmail.com>
2024-09-19 18:02:41 +0700

    rename _indexes to _default_indexes && add _custom_indexes, _default_int64_fields, _custom_int64_fields

--- domain/model/base_model.py
+++ domain/model/base_model.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from bson import Int64
 
+
 class _MyBaseModel_Index(BaseModel):
     """
     this attributes is same as pymongo.collection.Collection.create_index() args
@@ -9,14 +10,19 @@ class _MyBaseModel_Index(BaseModel):
     keys: list[tuple] = []
     unique: bool = False
 
+
 class MyBaseModel(BaseModel):
     """
     id field already indexed by default, but it need to be indexed manually if you set the _indexes field.
     """
+
     _coll_name: str = ""
-    _indexes: list[_MyBaseModel_Index] = [
+    _default_indexes: list[_MyBaseModel_Index] = [
         _MyBaseModel_Index(keys=[("id", 1)], unique=True)
     ]
+    _custom_indexes: list[_MyBaseModel_Index] = []
+    _default_int64_fields: list[str] = ["created_at", "updated_at"]
+    _custom_int64_fields: list[str] = []
 
     id: str = ""
     created_at: int = 0
@@ -24,9 +30,9 @@ class MyBaseModel(BaseModel):
     created_by: str = ""
     updated_by: str = ""
 
-    def model_dump(self, int64_fields: list = ["created_at", "updated_at"], **kwargs) -> dict:
+    def model_dump(self, **kwargs) -> dict:
         data = super().model_dump(**kwargs)
-        for field in int64_fields:
+        for field in self._custom_int64_fields + self._default_int64_fields:
             if field in data:
                 data[field] = Int64(data[field])
         return data
